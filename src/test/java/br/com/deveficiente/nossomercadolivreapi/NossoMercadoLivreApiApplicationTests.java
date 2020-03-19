e43ebc843869f1ad7ba9fefa860a9a5917fad662
JORGE GALVAO <jorgeluizjava@gmail.com>
2020-03-19 16:48:42 -0300

    Criação do projeto

---
+++ src/test/java/br/com/deveficiente/nossomercadolivreapi/NossoMercadoLivreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.nossomercadolivreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoMercadoLivreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
