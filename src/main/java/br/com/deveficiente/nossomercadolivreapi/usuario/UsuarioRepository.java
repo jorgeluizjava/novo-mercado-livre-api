df295fff3179446a955cc7cca15546dfcb1166ec
JORGE GALVAO <jorgeluizjava@gmail.com>
2020-03-22 15:05:48 -0300

    Alterando UUID para Long

--- src/main/java/br/com/deveficiente/nossomercadolivreapi/usuario/UsuarioRepository.java
+++ src/main/java/br/com/deveficiente/nossomercadolivreapi/usuario/UsuarioRepository.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface UsuarioRepository extends CrudRepository<Usuario, UUID> {
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 
     Optional<Usuario> findByLogin(String login);
 }
