5f700584a6403227ec8ca9db123f234b67fc3d35
JORGE GALVAO <jorgeluizjava@gmail.com>
2020-03-22 19:04:47 -0300

    Alteração do UUID para Long

--- src/main/java/br/com/deveficiente/nossomercadolivreapi/usuario/UsuarioRepository.java
+++ src/main/java/br/com/deveficiente/nossomercadolivreapi/usuario/UsuarioRepository.java
@@ -3,9 +3,7 @@ package br.com.deveficiente.nossomercadolivreapi.usuario;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 @Repository
-public interface UsuarioRepository extends CrudRepository<Usuario, UUID> {
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 
 }
